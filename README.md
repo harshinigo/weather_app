3853a5bedf48e6878cefcd616e9a4c7b19e8eb7d
harshinigo <gowda21h@gmail.com>
2022-11-13 22:34:47 +1000

    update

---
+++ README.md
@@ -0,0 +1,2 @@
+# Weather-app
+2nd project
---
+++ script.js
@@ -0,0 +1,85 @@
+let weather = {
+  apiKey: "20957daa73739ee04a5f325baf22699d",
+  fetchWeather: function (city) {
+    fetch(
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+        city +
+        "&units=metric&appid=" +
+        this.apiKey
+    )
+      .then((response) => {
+        if (!response.ok) {
+          alert("No weather found.");
+          throw new Error("No weather found.");
+        }
+        return response.json();
+      })
+      .then((data) => this.displayWeather(data));
+  },
+  displayWeather: function (data) {
+    const { name } = data;
+    const { icon, description } = data.weather[0];
+    const { temp, humidity } = data.main;
+    const { speed } = data.wind;
+
+    document.querySelector(".city").innerText = "Weather in " + name;
+    document.querySelector(".icon").src =
+      "https://openweathermap.org/img/wn/" + icon + ".png";
+    document.querySelector(".description").innerText = description;
+    document.querySelector(".temp").innerText = temp + "°C";
+    document.querySelector(".humidity").innerText =
+      "Humidity: " + humidity + "%";
+    document.querySelector(".wind").innerText =
+      "Wind speed: " + speed + " km/h";
+    document.querySelector(".weather").classList.remove("loading");
+    document.body.style.backgroundImage =
+      "url('https://source.unsplash.com/1600x900/?" + name + "')";
+  },
+  search: function () {
+    this.fetchWeather(document.querySelector(".search-bar").value);
+  },
+};
+
+document.querySelector(".search button").addEventListener("click", function () {
+  weather.search();
+});
+
+document
+  .querySelector(".search-bar")
+  .addEventListener("keyup", function (event) {
+    if (event.key == "Enter") {
+      weather.search();
+    }
+  });
+
+weather.fetchWeather("Mysore");
+
+//fetching news data from a website providing api
+
+const newsApiKey = "54e6adeffe214d468d4772603a7eea6a";
+function fetchNews(city) {
+  fetch("https://newsapi.org/v2/everything?q=" + city + "&apiKey=" + newsApiKey)
+    .then((response) => {
+      if (!response.ok) {
+        alert("No News found.");
+        throw new Error("No News found.");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data.articles[0]);
+      const html = data.articles.map((user) => {
+        return `<p>Title: ${user.title}</p>
+        <p>Description: ${user.description}</p>
+        <p>url: ${user.url}</p>`;
+      });
+      console.log(html);
+      document
+        .querySelector()
+        .insertAdjacentHTML("afterbegin", "<h1>Hello</h1>");
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+fetchNews("Mysore");
